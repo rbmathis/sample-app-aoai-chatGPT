07a76a8496bdb24a29b440581f46f96b3479395c
Bart Mathis (MSFT) <rmathis@microsoft.com>
2024-02-15 10:17:50 -0600

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/160-1608170_army-war-college-us-army-war-college-crest.png.svg";
 import { CopyRegular, ShareRegular } from "@fluentui/react-icons";
 import { Dialog, Stack, TextField, ICommandBarStyles, IButtonStyles } from "@fluentui/react";
 import { useContext, useEffect, useState } from "react";
